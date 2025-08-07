7ac61c69c663cf90446f9e0628136bfd85565933
Apurva <apurvapramodnair@gmail.com>
2025-08-07 12:53:36 +0530

    Modified commit for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,15 +13,24 @@ import { AppointmentModule } from './appointment/appointment.module';
       isGlobal: true, 
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: false,
-    }),
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: false,
+  ssl: true, 
+  extra:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+}),
     AuthModule,
     DoctorModule,
     PatientModule,
