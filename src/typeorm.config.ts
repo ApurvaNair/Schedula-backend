713155cc62e53872295b72cd9746aecdcc6e2c41
Apurva <apurvapramodnair@gmail.com>
2025-08-07 12:58:53 +0530

    Modified commit for deployment

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,12 +1,12 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'root',
-  database: 'schedula',
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: false, 
-};
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'root',
+//   database: 'schedula',
+//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//   synchronize: false, 
+// };
